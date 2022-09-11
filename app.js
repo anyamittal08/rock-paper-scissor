2e5951a3e245156063a6295e1487df7542e87c23
anyamittal08 <anyamittal08@gmail.com>
2022-09-11 15:03:11 -0400

    console rock paper scissors game

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const CHOICE_ARR = ['rock', 'paper', 'scissors']
+const CHOICE_ARR = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
     let randomIndex = Math.floor(Math.random() * CHOICE_ARR.length);
@@ -6,26 +6,43 @@ const getComputerChoice = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-    let casedPlayerSeleciton = playerSelection.toLowerCase()
-    let result;
+    let casedPlayerSelection = playerSelection.toLowerCase();
+    let winner;
     if (playerSelection === computerSelection) {
-        result = 'It\'s a draw!'
-    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection==='rock')) {
-        result = `You lose! ${computerSelection} beats ${playerSelection}`
+        console.log("This round is a draw")
+    } else if ((casedPlayerSelection === 'rock' && computerSelection === 'paper') || (casedPlayerSelection === 'paper' && computerSelection === 'scissors') || (casedPlayerSelection === 'scissors' && computerSelection==='rock')) {
+        winner = 'computer';
+        console.log(`You lose this round! ${computerSelection} beats ${playerSelection}`);
     } else {
-        result = `You win! ${playerSelection} beats ${computerSelection}`
+        winner = 'player';
+        console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
     }
+
+    return winner;
+};
+
+const game = () => {
+    let playerSelection;
+    let computerSelection;
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let result;
+
+    for (let i=0; i<5; i++) {
+        playerSelection = prompt('rock, paper, scissors?');
+        computerSelection = getComputerChoice();
+        let currentRoundWinner = playRound(playerSelection, computerSelection);
+        if (currentRoundWinner) {
+            if (currentRoundWinner === 'player') playerPoints++;
+            else if (currentRoundWinner === 'computer') computerPoints++
+        }
+    }
+
+    if (playerPoints > computerPoints) result = 'player wins the game!'
+    else if (playerPoints < computerPoints) result = 'computer wins the game!'
+    else result = 'it\'s a draw!'
+
     return result;
 }
 
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
-
-playerSelection = "paper";
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
-
-playerSelection = "scissors";
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+console.log(game());
